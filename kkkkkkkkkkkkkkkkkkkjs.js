ace7afe050b217e722d58c0a1699246cde839f48
userrezzz <96476183+userrezzz@users.noreply.github.com>
2022-01-22 19:38:10 +0700

    Update kkkkkkkkkkkkkkkkkkkjs.js

--- kkkkkkkkkkkkkkkkkkkjs.js
+++ kkkkkkkkkkkkkkkkkkkjs.js
@@ -1,12 +1,4 @@
-/*
-Copyright (c) 2015, Yahoo! Inc. All rights reserved.
-Copyrights licensed under the New BSD License.
-See the accompanying LICENSE file for terms.
 
-Authors: Nera Liu <neraliu@yahoo-inc.com>
-         Adonis Fung <adon@yahoo-inc.com>
-         Albert Yu <albertyu@yahoo-inc.com>
-*/
 (function() {
 
     var filter = xssFilters._privFilters;
